6a28e14ceab7ea0656e8611a9dc2335d3cc2768f
iansmorrison <12200552+iansmorrison@users.noreply.github.com>
2021-07-06 14:50:18 +0800

    Tweaked

--- mwax-cbf-cuda/mwax_cbf_cuda.cu
+++ mwax-cbf-cuda/mwax_cbf_cuda.cu
@@ -276,7 +276,7 @@ __global__ void mwax_lookup_delay_gains_delay_pairs_kernel(const int32_t* delays
   int i;
   for (i=0; i<num_ffts; i++)  // each FFT will use the same delay gain gradient
   {
-    delay_gains[gains_idx + i*fft_length] = delay_gain;
+    //delay_gains[gains_idx + i*fft_length] = delay_gain;
   }
   return;
 }
