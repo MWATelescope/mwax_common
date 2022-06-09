c8ea8538e059dd47c78a0b4ae41589b46895dbb3
iansmorrison <ian.morrison@curtin.edu.au>
2022-06-09 16:37:50 +0800

    Tweaked

--- mwax-cbf-cuda/mwax_cbf_cuda.h
+++ mwax-cbf-cuda/mwax_cbf_cuda.h
@@ -10,7 +10,7 @@ char * mwax_cuda_get_device_name (int index);
 
 int mwax_array_weight_complex(float* weights, float* output, unsigned rows, unsigned columns, cudaStream_t stream);
 int mwax_fast_complex_multiply(float* input, float* output, unsigned size, cudaStream_t stream);
-int mwax_lookup_all_delay_gains(int16_t* delays, cuFloatComplex* delay_lut, cuFloatComplex* delay_gains, unsigned paths, unsigned fft_length, unsigned num_ffts, cudaStream_t stream);
+int mwax_lookup_all_delay_gains(int16_t* delays, float complex* delay_lut, float complex* delay_gains, unsigned paths, unsigned fft_length, unsigned num_ffts, cudaStream_t stream);
 
 
 int mwax_transpose_to_xGPU(float complex* input, float complex* output, unsigned rows, unsigned columns, cudaStream_t stream);
